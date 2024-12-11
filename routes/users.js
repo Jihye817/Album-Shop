faf99c06e79374af9c5cc3a98d8dbdeaa4d83f9d
jihye <wlgul817@gmail.com>
2024-12-11 11:39:01 +0900

    ✨ feat: 유효성 검사 추가

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,44 @@
 const express = require("express");
 const router = express.Router();
 const conn = require("../mariadb");
+const { validationResult, body } = require("express-validator");
 
 router.use(express.json());
 
-//회원가입
-router.post("/join", (req, res) => {
-  const {email, name, password} = req.body;
+const validate = (req, res, next) => {
+  const err = validationResult(req);
+  if (err.isEmpty()) {
+    return next();
+  } else {
+    return res.status(400).json(err.array());
+  }
+};
 
-  let sql = "INSERT INTO users (email, name, password) VALUES (?, ?, ?)";
-  let values = [email, name, password];
-  conn.query(sql, values, (err, results) => {
-    if(err) {
-      return res.status(400).end();
-    }
-    res.status(201).json(results);
-  })
-});
+//회원가입
+router.post(
+  "/join",
+  [
+    body("email").notEmpty().isEmail().withMessage("이메일을 확인해주세요"),
+    body("name").notEmpty().isString().withMessage("이름을 확인해주세요"),
+    body("password")
+      .notEmpty()
+      .isString()
+      .withMessage("비밀번호를 확인해주세요"),
+    validate,
+  ],
+  (req, res) => {
+    const { email, name, password } = req.body;
+
+    let sql = "INSERT INTO users (email, name, password) VALUES (?, ?, ?)";
+    let values = [email, name, password];
+    conn.query(sql, values, (err, results) => {
+      if (err) {
+        return res.status(400).end();
+      }
+      res.status(201).json(results);
+    });
+  }
+);
 
 //로그인
 router.post("/login", (req, res) => {});
