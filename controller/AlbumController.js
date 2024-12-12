1546f8f3b85d68aaee65ab3834dc24531dd8145c
jihye <wlgul817@gmail.com>
2024-12-12 11:29:45 +0900

    ✨ feat: 카테고리별 앨범 조회

--- controller/AlbumController.js
+++ controller/AlbumController.js
@@ -31,6 +31,22 @@ const albumDetail = (req, res) => {
   });
 };
 
-const albumsByCategory = (req, res) => {};
+const albumsByCategory = (req, res) => {
+  const { category_id } = req.query;
+  const sql = "SELECT * FROM albums WHERE category_id = ?";
+  conn.query(sql, category_id, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results.length !== 0) {
+      res.status(StatusCodes.OK).json(results[0]);
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "해당 카테고리의 앨범 데이터가 없습니다." });
+    }
+  });
+};
 
 module.exports = { allAlbums, albumDetail, albumsByCategory };
