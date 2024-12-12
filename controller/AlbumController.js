08de8de1936d6d9ff964bbf4069b3ba3b29e2bdf
jihye <wlgul817@gmail.com>
2024-12-12 11:01:35 +0900

    ✨ feat: 앨범 상세 조회 구현 및 유효성 검사 추가

--- controller/AlbumController.js
+++ controller/AlbumController.js
@@ -12,7 +12,24 @@ const allAlbums = (req, res) => {
   });
 };
 
-const albumDetail = (req, res) => {};
+const albumDetail = (req, res) => {
+  const { id } = req.params;
+
+  const sql = "SELECT * FROM albums WHERE id = ?";
+  conn.query(sql, id, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results[0]) {
+      res.status(StatusCodes.OK).json(results[0]);
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "해당 id의 앨범 데이터가 없습니다." });
+    }
+  });
+};
 
 const albumsByCategory = (req, res) => {};
 
--- routes/albums.js
+++ routes/albums.js
@@ -1,16 +1,35 @@
 const express = require("express");
+const router = express.Router();
+const { StatusCodes } = require("http-status-codes");
+const { validationResult, param } = require("express-validator");
 const {
   allAlbums,
   albumsByCategory,
   albumDetail,
 } = require("../controller/AlbumController");
-const router = express.Router();
+
 router.use(express.json());
+
+const validate = (req, res, next) => {
+  const err = validationResult(req);
+  if (err.isEmpty()) {
+    return next();
+  } else {
+    res.status(StatusCodes.BAD_REQUEST).json(err.array());
+  }
+};
 
 router.get("/", allAlbums);
 
 router.get("/", albumsByCategory);
 
-router.get("/:id", albumDetail);
+router.get(
+  "/:id",
+  [
+    param("id").notEmpty().isInt().withMessage("id값을 확인해주세요."),
+    validate,
+  ],
+  albumDetail
+);
 
 module.exports = router;
