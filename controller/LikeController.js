b79ff7d7863814dc760b87736015eae71d84a00e
jihye <wlgul817@gmail.com>
2024-12-16 11:13:01 +0900

    ✨ feat: 좋아요 삭제 구현

--- controller/LikeController.js
+++ controller/LikeController.js
@@ -18,11 +18,22 @@ const addLike = (req, res) => {
 };
 
 const deleteLike = (req, res) => {
+  const liked_album_id = req.params.id;
+  const { user_id } = req.body;
+
   let sql = "DELETE FROM likes WHERE user_id = ? AND liked_album_id = ?";
+  let values = [user_id, liked_album_id];
   conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results.affectedRows !== 0) {
+      res.status(StatusCodes.OK).end();
+    } else {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "해당하는 데이터가 없습니다." });
     }
   });
 };
