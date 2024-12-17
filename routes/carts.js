75f245208107ee1da9760e768ccb7bec360896d7
jihye <wlgul817@gmail.com>
2024-12-17 12:03:44 +0900

    ⚡️ feat: carts.js에 유효성 검사 추가

--- routes/carts.js
+++ routes/carts.js
@@ -1,13 +1,50 @@
 const express = require("express");
-const { addToCart, getCartItems, deleteCartItem, selectedCartItem } = require("../controller/CartController");
 const router = express.Router();
+const { validationResult, body, param } = require("express-validator");
+const {
+  addToCart,
+  getCartItems,
+  deleteCartItem,
+} = require("../controller/CartController");
+const { StatusCodes } = require("http-status-codes");
+
 router.use(express.json());
+
+const validate = (req, res, next) => {
+  const err = validationResult(req);
+  if (err.isEmpty()) {
+    return next();
+  } else {
+    res.status(StatusCodes.BAD_REQUEST).json(err.array());
+  }
+};
 
 router
   .route("/")
-  .post(addToCart)
-  .get(getCartItems);
+  .post(
+    [
+      body("album_id").isEmpty().withMessage("album_id 값을 확인해주세요"),
+      body("quantity")
+        .isEmpty()
+        .isInt()
+        .withMessage("quantity 값을 확인해주세요"),
+      body("user_id").isEmpty().withMessage("user_id 값을 확인해주세요"),
+      validate,
+    ],
+    addToCart
+  )
+  .get(
+    [
+      body("user_id").exists().isInt().withMessage("user_id 값을 확인해주세요"),
+      validate,
+    ],
+    getCartItems
+  );
 
-router.delete("/:id", deleteCartItem);
+router.delete(
+  "/:id",
+  [param("id").isEmpty().withMessage("id 값을 확인해주세요"), validate],
+  deleteCartItem
+);
 
 module.exports = router;
