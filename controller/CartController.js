b5d2b82bb6745f9c9cad171d81af55b52b155cb7
jihye <wlgul817@gmail.com>
2024-12-17 10:33:26 +0900

    ✨ feat: 장바구니 아이템 삭제 구현

--- controller/CartController.js
+++ controller/CartController.js
@@ -3,9 +3,9 @@ const conn = require("../mariadb");
 
 const addToCart = (req, res) => {
   let { album_id, quantity, user_id } = req.body;
+
   let sql =
     "INSERT INTO cartItems (album_id, quantity, user_id) VALUES (?, ?, ?)";
-
   let values = [album_id, quantity, user_id];
   conn.query(sql, values, (err, results) => {
     if (err) {
@@ -18,9 +18,9 @@ const addToCart = (req, res) => {
 
 const getCartItems = (req, res) => {
   let { user_id } = req.body;
+
   let sql = `SELECT cartItems.id, album_id, title, summary, quantity, price FROM cartItems
     LEFT JOIN albums ON cartItems.album_id = albums.id WHERE user_id = ?`;
-
   conn.query(sql, user_id, (err, results) => {
     if (err) {
       console.log(err);
@@ -31,7 +31,16 @@ const getCartItems = (req, res) => {
 };
 
 const deleteCartItem = (req, res) => {
-  res.json("장바구니 아이템 삭제");
+  let { id } = req.params;
+
+  let sql = "DELETE FROM cartItems WHERE id = ?";
+  conn.query(sql, id, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    res.status(StatusCodes.OK).json(results);
+  });
 };
 
 module.exports = { addToCart, getCartItems, deleteCartItem };
